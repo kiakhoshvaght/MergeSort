5457320835b7780a9f3311e296bc1bd94b44dc44
Kia Khoshvaght <kiakhoshvaght@gmail.com>
2019-02-26 11:24:25 +0330

    Natural merge sort added.

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,9 +1,53 @@
 import Utils.Log;
 
-import java.util.Arrays;
 
 public class MergeSort {
     public static final String TAG = MergeSort.class.getName();
+
+    public static boolean isSorted(int[] array) {
+        for (int i = 1; i < array.length; i += 1) {
+            if (array[i - 1] > array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void naturalMergeSort(int[] array) {
+        int left = 0;
+        int right = array.length;
+        int i = left;
+        int j = 0;
+
+        while (true) {
+            i = 0;
+            while (i < array.length) {
+                if (i == array.length - 1) {
+                    break;
+                } else if (array[i] > array[i + 1]) {
+                    break;
+                }
+                i++;
+            }
+
+            j = i + 1;
+
+            while (j < array.length) {
+                if (j == array.length - 1) {
+                    break;
+                } else if (array[j]>array[j + 1]) {
+                    break;
+                }
+                j++;
+            }
+            merge(array, left, i, j);
+            left = 0;
+
+            if (isSorted(array)) {
+                break;
+            }
+        }
+    }
 
     private static void mergeWithAux(int[] array, int left, int middle, int right) {
         int[] aux = new int[array.length];
@@ -37,9 +81,6 @@ public class MergeSort {
             else
                 aux[z++] = array[j++];
         }
-        System.out.println(Arrays.toString(array));
-        System.out.println("start = "+left+" mid = "+middle+" end = "+right);
-        System.out.println(Arrays.toString(aux)+"\n");
         System.arraycopy(aux, left, array, left, right-left+1);
     }
 
@@ -54,18 +95,18 @@ public class MergeSort {
     }
 
     static void topDownMergeSort(int array[], int left, int right) {
-        Log.info(TAG, "FUNCTION : topDownMergeSort");
+//        Log.info(TAG, "FUNCTION : topDownMergeSort");
         if (left < right) { //Checking if we have divided arrays enough
-            Log.info(TAG, "FUNCTION : topDownMergeSort => Divided arrays before sorting: ");
+//            Log.info(TAG, "FUNCTION : topDownMergeSort => Divided arrays before sorting: ");
             //Calculating separation points
             int middle = left + (right - left) / 2;
 
-            for (int i = left; i <= right; i++) {
-                System.out.print(array[i] + ",");
-                if (i == middle)
-                    System.out.print(" | ");
-            }
-            System.out.println(" ");
+//            for (int i = left; i <= right; i++) {
+//                System.out.print(array[i] + ",");
+//                if (i == middle)
+//                    System.out.print(" | ");
+//            }
+//            System.out.println(" ");
 
             //Recursively calling this function for two divided arrays
             topDownMergeSort(array, left, middle);
@@ -76,29 +117,29 @@ public class MergeSort {
     }
 
     static void topDownMergeSortWithAux(int array[], int left, int right) {
-        Log.info(TAG, "FUNCTION : topDownMergeSort");
+//        Log.info(TAG, "FUNCTION : topDownMergeSort");
         if (left < right) { //Checking if we have divided arrays enough
-            Log.info(TAG, "FUNCTION : topDownMergeSort => Divided arrays before sorting: ");
+//            Log.info(TAG, "FUNCTION : topDownMergeSort => Divided arrays before sorting: ");
             //Calculating separation points
             int middle = left + (right - left) / 2;
 
-            for (int i = left; i <= right; i++) {
-                System.out.print(array[i] + ",");
-                if (i == middle)
-                    System.out.print(" | ");
-            }
-            System.out.println(" ");
+//            for (int i = left; i <= right; i++) {
+//                System.out.print(array[i] + ",");
+//                if (i == middle)
+//                    System.out.print(" | ");
+//            }
+//            System.out.println(" ");
 
             //Recursively calling this function for two divided arrays
-            topDownMergeSort(array, left, middle);
-            topDownMergeSort(array, middle + 1, right);
+            topDownMergeSortWithAux(array, left, middle);
+            topDownMergeSortWithAux(array, middle + 1, right);
             //Merging them
             mergeWithAux(array, left, middle, right);
         }
     }
 
     static void merge(int arr[], int left, int middle, int right) {
-        Log.info(TAG, "FUNCTION : merge");
+//        Log.info(TAG, "FUNCTION : merge");
         //Calculating sub arrays
         int i, j, k;
         int n1 = middle - left + 1;
@@ -142,21 +183,50 @@ public class MergeSort {
         }
     }
 
+    static void stringToIntArray(String string, int[] array){
+        for (int i = 0; i < string.length(); i++) {
+            array [i] = (int) string.charAt(i);
+        }
+    }
+
     public static void main(String[] args) {
         Log.info(TAG, "FUNCTION : main");
 
-        int TDTest1[] = {10, 34, 5, 12, 22, 69, 3, 1, 11, 13, 8};
+        int TDTest1[] ={3, 18, 11, 13, 2, 33 , 5,15,0,7,4,1,16};
         topDownMergeSort(TDTest1, 0, TDTest1.length - 1);
-        Log.info(TAG, "FUNCTION : main => TDTest1 result: ");
+        Log.info(TAG, "FUNCTION : main => Top Down Sort result: ");
         for (int i = 0; i < TDTest1.length; i++)
             System.out.print(TDTest1[i] + ",");
         System.out.println(" ");
 
-        int BUTest1[] = {3, 18, 11, 13, 2, 33 , 5,15,0,7,4,1};
+        String name = "NILOOFARKALANTARI";
+        int TDStringTest1[] = new int[name.length()];
+        stringToIntArray(name,TDStringTest1);
+        topDownMergeSort(TDStringTest1, 0, TDStringTest1.length - 1);
+        Log.info(TAG, "FUNCTION : main => Top Down Sort for string result: ");
+        for (int i = 0; i < TDStringTest1.length; i++)
+            System.out.print((char)TDStringTest1[i] + ",");
+        System.out.println(" ");
+
+        int BUTest1[] = {3, 18, 11, 13, 2, 33 , 5,15,0,7,4,1,16};
         bottomUpMergeSort(BUTest1);
-        Log.info(TAG, "FUNCTION : main => BUTest1 result: ");
+        Log.info(TAG, "FUNCTION : main => Bottom Up Test result: ");
         for (int i = 0; i < BUTest1.length; i++)
             System.out.print(BUTest1[i] + ",");
+        System.out.println(" ");
+
+        int TDUTest1[] = {3, 18, 11, 13, 2, 33 , 5,15,0,7,4,1,16};
+        topDownMergeSortWithAux(TDUTest1,0,TDUTest1.length-1);
+        Log.info(TAG, "FUNCTION : main => Top Down With Aux result: ");
+        for (int i = 0; i < BUTest1.length; i++)
+            System.out.print(TDUTest1[i] + ",");
+        System.out.println(" ");
+
+        int NMTest1[] = {3, 18, 11, 13, 2, 33 , 5,15,0,7,4,1,16};
+        naturalMergeSort(NMTest1);
+        Log.info(TAG, "FUNCTION : main => Natural merge sort result: ");
+        for (int i = 0; i < NMTest1.length; i++)
+            System.out.print(NMTest1[i] + ",");
         System.out.println(" ");
     }
 
